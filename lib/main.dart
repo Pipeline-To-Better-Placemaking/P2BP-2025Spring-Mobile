f96e175f31d2be971eecdbb12fb5a315519e043c
Dominic <domcasta09@gmail.com>
2024-11-22 17:07:44 -0500

    UPDATING WITH PAGES, DO NOT MERGE ONTO MAIN. TEMPORARY UNTIL IMPLEMENTED WITH HOME SCREEN.

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,104 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
-import 'forgot_password_page.dart';
-import 'reset_password_page.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  static const customSwatch = MaterialColor(
+    0xFFFF5252,
+    <int, Color>{
+      50: Color(0xFFFFEBEE),
+      100: Color(0xFFFFCDD2),
+      200: Color(0xFFEF9A9A),
+      300: Color(0xFFE57373),
+      400: Color(0xFFEF5350),
+      500: Color(0xFFFF5252),
+      600: Color(0xFFE53935),
+      700: Color(0xFFD32F2F),
+      800: Color(0xFFC62828),
+      900: Color(0xFFB71C1C),
+    },
+  );
 
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pipeline to Better Placemaking',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: customSwatch,
       ),
-      home: const MyHomePage(title: 'P2BP Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class ObscuredTextBox extends StatelessWidget {
+  const ObscuredTextBox({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 100,
+      width: 250,
+      child: TextField(
+        style: TextStyle(color: Colors.white),
+        cursorColor: Colors.white10,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white70),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          labelText: 'Project Name',
+          labelStyle: TextStyle(
+            color: Colors.white60,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ObscuredTextBox2 extends StatelessWidget {
+  const ObscuredTextBox2({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      child: TextField(
+        style: TextStyle(color: Colors.white),
+        maxLines: 4,
+        minLines: 3,
+        cursorColor: Colors.white10,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white70),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          labelText: 'Project Description',
+          floatingLabelAlignment: FloatingLabelAlignment.start,
+          labelStyle: TextStyle(
+            color: Colors.white60,
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -37,30 +106,232 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text("P2BP"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: () {
-                Navigator.push( // pushes ForgotPasswordPage onto nav stack
-                    context,
-                    MaterialPageRoute(builder: (context) => const ForgotPasswordPage())
-                );
-              },
-              child: const Text('Forgot Password')
+      body: SlidingUpPanel(
+        backdropEnabled: true, //darken background if panel is open
+        color: Colors
+            .transparent, //necessary if you have rounded corners for panel
+        /// panel itself
+        panel: Container(
+          decoration: BoxDecoration(
+            // background color of panel
+            color: Colors.blueAccent,
+            // rounded corners of panel
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24.0),
+              topRight: Radius.circular(24.0),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.push( // pushes ResetPasswordPage onto nav stack
-                    context,
-                    MaterialPageRoute(builder: (context) => const ResetPasswordPage())
-                );
-              },
-              child: const Text('Reset password')
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              BarIndicator(),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(bottom: 20),
+                child: Text(
+                  "Edit Project",
+                  style: TextStyle(
+                      color: Colors.yellow[700],
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.only(right: 35),
+                    margin: const EdgeInsets.only(left: 20, right: 20),
+                    child: ObscuredTextBox(),
+                  ),
+                  CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: Colors.white,
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: ObscuredTextBox2(),
+              ),
+              Row(
+                children: [
+                  Container(
+                    alignment: Alignment.topLeft,
+                    margin: const EdgeInsets.only(top: 75, left: 20, right: 5),
+                    child: FilledButton(
+                      child: const Text('Update Map'),
+                      style: FilledButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.yellow[600],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 75, left: 5, right: 20),
+                    child: FilledButton(
+                      child: const Text('Delete Project'),
+                      style: FilledButton.styleFrom(
+                        elevation: 100,
+                        shadowColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        backgroundColor: Colors.black,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                'Cancel',
+              ),
+            ],
+          ),
+        ),
+
+        /// header of panel while collapsed
+        collapsed: Container(
+          decoration: BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24.0),
+              topRight: Radius.circular(24.0),
             ),
-          ],
+          ),
+          child: Column(
+            children: [
+              BarIndicator(),
+              Center(
+                child: Text(
+                  "Temporary until Project Home Page Integration",
+                  style: TextStyle(color: Colors.yellow[700]),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// widget behind panel
+
+        body: Center(
+          child: ElevatedButton(
+            child: const Text('Go to Results Page'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Results Page'),
+      ),
+      body: SlidingUpPanel(
+        backdropEnabled: true, //darken background if panel is open
+        color: Colors
+            .transparent, //necessary if you have rounded corners for panel
+        /// panel itself
+        panel: Container(
+          decoration: BoxDecoration(
+            // background color of panel
+            color: Colors.blueAccent,
+            // rounded corners of panel
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24.0),
+              topRight: Radius.circular(24.0),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              BarIndicator(),
+              Center(
+                child: Text(
+                  "Results",
+                  style: TextStyle(
+                      color: Colors.yellow[700],
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// header of panel while collapsed
+        collapsed: Container(
+          decoration: BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24.0),
+              topRight: Radius.circular(24.0),
+            ),
+          ),
+          child: Column(
+            children: [
+              BarIndicator(),
+              Center(
+                child: Text(
+                  "Results",
+                  style: TextStyle(color: Colors.yellow[700]),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// widget behind panel
+
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Back to Home/Edit Project'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BarIndicator extends StatelessWidget {
+  const BarIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        width: 40,
+        height: 5,
+        decoration: BoxDecoration(
+          color: Colors.white60,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
         ),
       ),
     );
--- lib/strings.dart
+++
@@ -1,7 +0,0 @@
-class Strings {
-  Strings._();
-  // static const String correctWay = "This is the way";
-
-  static const String forgotPasswordText =
-      'It happens. Just enter the email associated with your account and we\'ll send you a link to reset it.';
-}
