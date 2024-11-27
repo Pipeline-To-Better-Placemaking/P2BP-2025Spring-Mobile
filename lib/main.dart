c043dd2387146a3790cbf09e03fee549f9b8cc49
Dominic <domcasta09@gmail.com>
2024-11-27 17:12:18 -0500

    UPDATING WITH PAGES, DO NOT MERGE ONTO MAIN. TEMPORARY UNTIL IMPLEMENTED WITH HOME SCREEN. Adding some final touches Todo: fix project description text alignment, onpress cancel

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
     },
   );
 
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -52,11 +52,13 @@ class ObscuredTextBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return const SizedBox(
       height: 100,
-      width: 250,
+      width: 300,
       child: TextField(
+        maxLength: 60,
         style: TextStyle(color: Colors.white),
         cursorColor: Colors.white10,
         decoration: InputDecoration(
+          counterStyle: TextStyle(color: Colors.white70),
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white70),
           ),
@@ -64,6 +66,9 @@ class ObscuredTextBox extends StatelessWidget {
             borderSide: BorderSide(color: Colors.white),
           ),
           labelText: 'Project Name',
+          floatingLabelStyle: TextStyle(
+            color: Colors.white,
+          ),
           labelStyle: TextStyle(
             color: Colors.white60,
           ),
@@ -80,10 +85,12 @@ class ObscuredTextBox2 extends StatelessWidget {
     return const SizedBox(
       child: TextField(
         style: TextStyle(color: Colors.white),
+        maxLength: 240,
         maxLines: 4,
         minLines: 3,
         cursorColor: Colors.white10,
         decoration: InputDecoration(
+          counterStyle: TextStyle(color: Colors.white70),
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white70),
           ),
@@ -92,6 +99,9 @@ class ObscuredTextBox2 extends StatelessWidget {
           ),
           labelText: 'Project Description',
           floatingLabelAlignment: FloatingLabelAlignment.start,
+          floatingLabelStyle: TextStyle(
+            color: Colors.white,
+          ),
           labelStyle: TextStyle(
             color: Colors.white60,
           ),
@@ -145,13 +155,26 @@ class _MyHomePageState extends State<MyHomePage> {
                   Container(
                     alignment: Alignment.center,
                     padding: EdgeInsets.only(right: 35),
-                    margin: const EdgeInsets.only(left: 20, right: 20),
+                    margin: const EdgeInsets.only(left: 20),
                     child: ObscuredTextBox(),
                   ),
-                  CircleAvatar(
-                    radius: 30.0,
-                    backgroundColor: Colors.white,
-                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 27.0,
+                        backgroundColor: Colors.yellow[700],
+                        child: Center(
+                            child: Icon(Icons.add_photo_alternate, size: 37)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 3),
+                        child: Text(
+                          'Update Cover',
+                          style: TextStyle(color: Colors.yellow[700]),
+                        ),
+                      )
+                    ],
+                  )
                 ],
               ),
               Container(
@@ -163,9 +186,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   Container(
                     alignment: Alignment.topLeft,
                     margin: const EdgeInsets.only(top: 75, left: 20, right: 5),
-                    child: FilledButton(
-                      child: const Text('Update Map'),
+                    child: FilledButton.icon(
                       style: FilledButton.styleFrom(
+                        padding: EdgeInsets.only(left: 15, right: 10),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10.0),
                         ),
@@ -173,13 +196,16 @@ class _MyHomePageState extends State<MyHomePage> {
                         backgroundColor: Colors.yellow[600],
                       ),
                       onPressed: () {},
+                      label: const Text('Update Map'),
+                      icon: Icon(Icons.gps_fixed),
+                      iconAlignment: IconAlignment.end,
                     ),
                   ),
                   Container(
                     margin: const EdgeInsets.only(top: 75, left: 5, right: 20),
-                    child: FilledButton(
-                      child: const Text('Delete Project'),
+                    child: FilledButton.icon(
                       style: FilledButton.styleFrom(
+                        padding: EdgeInsets.only(left: 15, right: 10),
                         elevation: 100,
                         shadowColor: Colors.black,
                         shape: RoundedRectangleBorder(
@@ -188,13 +214,26 @@ class _MyHomePageState extends State<MyHomePage> {
                         backgroundColor: Colors.black,
                       ),
                       onPressed: () {},
+                      label: const Text('Delete Project'),
+                      icon: Icon(Icons.delete),
+                      iconAlignment: IconAlignment.end,
                     ),
                   ),
                 ],
               ),
-              Text(
-                'Cancel',
-              ),
+              InkWell(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Center(
+                      child: Text(
+                    'Cancel',
+                    style: TextStyle(fontSize: 16, color: Colors.yellow[700]),
+                  )),
+                ),
+                onTap: () {
+                  setState(() {});
+                },
+              )
             ],
           ),
         ),
@@ -326,12 +365,14 @@ class BarIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20),
-      child: Container(
-        width: 40,
-        height: 5,
-        decoration: BoxDecoration(
-          color: Colors.white60,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
+      child: Center(
+        child: Container(
+          width: 40,
+          height: 5,
+          decoration: BoxDecoration(
+            color: Colors.white60,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
         ),
       ),
     );
