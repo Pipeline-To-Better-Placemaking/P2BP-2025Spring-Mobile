7c09e11fa12085489ff5d43dff0028d3e1db3f6d
Ryan Giunta <RyanJGiunta@gmail.com>
2024-12-16 15:00:24 -0500

    added dart files and assets from feature/forgot-password-screen to new main

---
+++ lib/strings.dart
@@ -0,0 +1,7 @@
+class Strings {
+  Strings._();
+  // static const String correctWay = "This is the way";
+
+  static const String forgotPasswordText =
+      'It happens. Just enter the email associated with your account and we\'ll send you a link to reset it.';
+}
