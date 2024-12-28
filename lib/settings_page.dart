f5aa353c40d6621d2ecade19d79391bf0c8e3f19
Ryan Giunta <RyanJGiunta@gmail.com>
2024-12-28 11:38:05 -0500

    basic start of settings page

---
+++ lib/settings_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'theme.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Settings'),
+        ),
+
+
+        bottomNavigationBar: const BottomFloatingNavBar(),
+      ),
+    );
+  }
+}
