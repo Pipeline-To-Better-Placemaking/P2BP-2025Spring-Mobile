114d2bae584f3e7c4bc288da1f5c5d60914c5f50
Ryan Giunta <RyanJGiunta@gmail.com>
2025-01-09 05:05:29 -0500

    Refactor LoginScreen to implement toggle visibility
    Extract relevant parts of LoginScreen to their own StatefulWidget LoginForm in order allow implementation of toggling of password visibility. Having all the relevant fields in a form may also be helpful later when implementing actual login functionality.
    Change column near top of LoginScreen to ListView to fix scrolling issues.
    Remove vestigial main function and MyApp class from top of file.
    Small update to PasswordTextFormField in widgets.dart to enable toggle visibility usage.

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
+import 'widgets.dart';
 import 'theme.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login Screen',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginScreen(),
-    );
-  }
-}
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -31,10 +13,8 @@ class LoginScreen extends StatelessWidget {
           gradient: defaultGrad,
         ),
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+        child: ListView(
+          children: <Widget>[
             // Logo
             Center(
               child: Image.asset(
@@ -53,97 +33,7 @@ class LoginScreen extends StatelessWidget {
               textAlign: TextAlign.left,
             ),
             const SizedBox(height: 20),
-            // Email Input
-            const TextField(
-              decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(left: 10, right: 30),
-                  child: Opacity(
-                    opacity: 0.75,
-                    child: ImageIcon(AssetImage('assets/mail_icon.png'),
-                        color: Colors.white),
-                  ),
-                ),
-                labelText: 'Email Address',
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                filled: false,
-              ),
-            ),
-            const SizedBox(height: 10),
-            // Password Input
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 30),
-                  child: ImageIcon(
-                    AssetImage('assets/Lock.png'),
-                    color: Colors.white,
-                  ),
-                ),
-                labelText: 'Password',
-                labelStyle: const TextStyle(color: Colors.white),
-                focusedBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                border: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                filled: false,
-                suffixIcon: IconButton(
-                  icon: const Icon(
-                    Icons.visibility,
-                    color: Colors.grey,
-                  ),
-                  onPressed: () {
-                    // Toggle password visibility
-                  },
-                ),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                'Forgot?',
-                style: TextStyle(
-                  color: Color(0xFFFFCC00),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            // Login Button
-            ElevatedButton(
-              onPressed: () {
-                // Handle login logic
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFFFCC00),
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                ),
-              ),
-            ),
+            const LoginForm(),
             const SizedBox(height: 20),
             // OR Divider
             const Row(
@@ -155,9 +45,10 @@ class LoginScreen extends StatelessWidget {
                   child: Text(
                     'OR',
                     style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17),
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
                   ),
                 ),
                 Expanded(child: Divider(color: Colors.white)),
@@ -223,6 +114,141 @@ class LoginScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController(),
+      _passwordController = TextEditingController();
+  bool _obscureText = true;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // Email Input
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(left: 10, right: 30),
+                child: Opacity(
+                  opacity: 0.75,
+                  child: Icon(
+                    Icons.mail_outline,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              labelText: 'Email Address',
+              labelStyle: TextStyle(color: Colors.white),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              filled: false,
+            ),
+          ),
+          const SizedBox(height: 10),
+          // Password Input
+          PasswordTextFormField(
+            controller: _passwordController,
+            obscureText: _obscureText,
+            decoration: InputDecoration(
+              prefixIcon: const Padding(
+                padding: EdgeInsets.only(left: 10, right: 30),
+                child: Opacity(
+                  opacity: 0.75,
+                  child: Icon(
+                    Icons.lock_outline,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              labelText: 'Password',
+              labelStyle: const TextStyle(color: Colors.white),
+              focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              border: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              filled: false,
+              suffixIcon: IconButton(
+                icon: Icon(
+                  Icons.visibility,
+                  color: Colors.grey[400],
+                ),
+                onPressed: () {
+                  // Toggle password visibility
+                  setState(() {
+                    _obscureText = _obscureText ? false : true;
+                  });
+                },
+              ),
+            ),
+          ),
+          const Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              'Forgot?',
+              style: TextStyle(
+                color: Color(0xFFFFCC00),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          // Login Button
+          ElevatedButton(
+            onPressed: () {
+              // Handle login logic
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFFFFCC00),
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            child: const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
