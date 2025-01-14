a7be43fe0f81f12142c9712be55364c829bfca98
Ryan Giunta <RyanJGiunta@gmail.com>
2025-01-13 19:34:20 -0500

    Add page switch logic and scrolling TabBar

--- lib/project_comparison_page.dart
+++ lib/project_comparison_page.dart
@@ -2,19 +2,32 @@ import 'package:flutter/material.dart';
 import 'widgets.dart';
 import 'strings.dart';
 
-// Nonsense filler until backend is started
-typedef Project = Map<int, String>;
-final Project _projects = {
-  -1: 'Select Project',
-  0: 'Project Nona',
-  1: 'Project Eola',
-  2: 'Project Osceola',
-  3: 'Project Oviedo',
-  4: 'Project Orlando'
-};
-// End filler
+enum _PageNames { text, compare }
 
-enum PageNames { text, compare }
+// Nonsense filler until backend is started
+const List<String> _testNames = <String>[
+  'Acoustical Profile',
+  'Spatial Boundaries',
+  'Nature Prevalence',
+  'Lighting Profile',
+  'Absence Of Order Locator',
+  'People In Place',
+  'People In Motion',
+  'Community Survey',
+  'Identifying Access',
+  'Identifying Program',
+  'Section Cutter',
+];
+
+const List<String> _projects = [
+  'Project Nona',
+  'Project Eola',
+  'Project Osceola',
+  'Project Oviedo',
+  'Project Orlando'
+];
+// End filler
+const String _dropdownDefault = 'Select Project';
 
 class ProjectComparisonPage extends StatefulWidget {
   const ProjectComparisonPage({super.key});
@@ -24,11 +37,31 @@ class ProjectComparisonPage extends StatefulWidget {
 }
 
 class _ProjectComparisonPageState extends State<ProjectComparisonPage> {
-  PageNames currentPage = PageNames.text;
-  var pages = [
-    const _TextPage(),
-    const _CompareDataPage(),
+  static const _pages = [
+    _TextPage(),
+    _CompareDataPage(),
   ];
+  _PageNames currentPage = _PageNames.text;
+
+  // Needs to be initialized in initState() to pass callback, hence late
+  late final _DropdownRowForm _dropdownRowForm;
+
+  @override
+  void initState() {
+    super.initState();
+    _dropdownRowForm = _DropdownRowForm(pageChangeCallback);
+  }
+
+  /// Used to switch to project comparison view once projects are selected.
+  /// setState must be called in this class to work but controllers and logic
+  /// are within _DropdownRowForm, requiring this callback function.
+  void pageChangeCallback(_PageNames newPage) {
+    if (currentPage != newPage) {
+      setState(() {
+        currentPage = newPage;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,11 +77,14 @@ class _ProjectComparisonPageState extends State<ProjectComparisonPage> {
             child: Padding(
               padding: EdgeInsets.all(18),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  _DropdownRowForm(),
+                  // Permanent row with dropdowns at top
+                  _dropdownRowForm,
                   const SizedBox(height: 25),
-                  pages[currentPage.index],
+
+                  // Dynamically switches between text default and comparison
+                  _pages[currentPage.index],
                 ],
               ),
             ),
@@ -59,8 +95,13 @@ class _ProjectComparisonPageState extends State<ProjectComparisonPage> {
   }
 }
 
+// Form containing row which just has two dropdown menus of projects
 class _DropdownRowForm extends StatefulWidget {
-  const _DropdownRowForm({super.key});
+  /// Required callback function from parent for passing page to since using
+  /// setState() here would not update anything outside this Form.
+  final Function pageChangeCallback;
+
+  const _DropdownRowForm(this.pageChangeCallback, {super.key});
 
   @override
   State<_DropdownRowForm> createState() => _DropdownRowFormState();
@@ -71,22 +112,39 @@ class _DropdownRowFormState extends State<_DropdownRowForm> {
       _dropdownController2 = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+    // Start listening to changes to dropdown selection
+    _dropdownController1.addListener(_checkBothDropdownSelected);
+    _dropdownController2.addListener(_checkBothDropdownSelected);
+  }
+
+  // Passes compare page if two projects have been selected, else text page
+  void _checkBothDropdownSelected() {
+    if ((_dropdownController1.text.isNotEmpty &&
+            _dropdownController1.text != _dropdownDefault) &&
+        (_dropdownController2.text.isNotEmpty &&
+            _dropdownController2.text != _dropdownDefault)) {
+      widget.pageChangeCallback(_PageNames.compare);
+    } else {
+      widget.pageChangeCallback(_PageNames.text);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Form(
       child: Row(
         children: [
           Expanded(
-            flex: 45,
+            flex: 9,
             child: _ProjectDropdown(
               controller: _dropdownController1,
             ),
           ),
+          Spacer(flex: 1),
           Expanded(
-            flex: 5,
-            child: SizedBox(),
-          ),
-          Expanded(
-            flex: 45,
+            flex: 9,
             child: _ProjectDropdown(
               controller: _dropdownController2,
             ),
@@ -104,6 +162,7 @@ class _DropdownRowFormState extends State<_DropdownRowForm> {
   }
 }
 
+/// Widget for dropdown menu of projects, displayed twice at top of this screen
 class _ProjectDropdown extends StatefulWidget {
   final TextEditingController? controller;
 
@@ -144,40 +203,57 @@ class _ProjectDropdownState extends State<_ProjectDropdown> {
           ),
         ),
         textStyle: TextStyle(
-          fontSize: 16,
+          fontSize: 12,
           color: Colors.white,
         ),
         inputDecorationTheme: InputDecorationTheme(
           isDense: true,
           contentPadding: EdgeInsets.only(left: 9),
-          constraints: BoxConstraints.tight(Size.fromHeight(45)),
+          constraints: BoxConstraints.tight(Size.fromHeight(50)),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
             borderSide: BorderSide.none,
           ),
         ),
         trailingIcon: Icon(
-          Icons.keyboard_arrow_up_sharp,
+          Icons.arrow_drop_down,
           color: Colors.white60,
         ),
-        initialSelection: _projects[-1],
+        selectedTrailingIcon: Icon(
+          Icons.arrow_drop_up,
+          color: Colors.white60,
+        ),
+        initialSelection: _dropdownDefault,
         dropdownMenuEntries: dropdownMenuEntries,
       ),
     );
   }
 
-  // Constructs the list of entries for project dropdowns
+  /// Constructs the list of entries for project dropdowns
   // Primarily constructed this way to hopefully be easy to
   // use with the backend once that is started
   // This will almost certainly need to be changed significantly once backend
   List<DropdownMenuEntry<String>> _buildDropdownEntryList() {
     List<DropdownMenuEntry<String>> result = [];
 
-    for (int i = -1; i < _projects.length - 1; i++) {
+    // Default entry
+    result.add(
+      DropdownMenuEntry(
+        value: _dropdownDefault,
+        label: _dropdownDefault,
+        style: ButtonStyle(
+          foregroundColor: WidgetStatePropertyAll(Colors.white),
+          visualDensity: VisualDensity(vertical: -2.5),
+        ),
+      ),
+    );
+
+    // Loops through all projects
+    for (var project in _projects) {
       result.add(
         DropdownMenuEntry(
-          value: _projects[i]!,
-          label: _projects[i]!,
+          value: project,
+          label: project,
           style: ButtonStyle(
             foregroundColor: WidgetStatePropertyAll(Colors.white),
             visualDensity: VisualDensity(vertical: -2.5),
@@ -190,6 +266,7 @@ class _ProjectDropdownState extends State<_ProjectDropdown> {
   }
 }
 
+/// Default lower/middle section of page with text instructions on how to use.
 class _TextPage extends StatelessWidget {
   const _TextPage({super.key});
 
@@ -205,6 +282,7 @@ class _TextPage extends StatelessWidget {
   }
 }
 
+/// Page displaying data from tests common to both selected projects.
 class _CompareDataPage extends StatefulWidget {
   const _CompareDataPage({super.key});
 
@@ -215,7 +293,96 @@ class _CompareDataPage extends StatefulWidget {
 class _CompareDataPageState extends State<_CompareDataPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(//TODO
-        );
+    return _TestNavigationTabBar();
+  }
+}
+
+class _TestNavigationTabBar extends StatefulWidget {
+  const _TestNavigationTabBar({super.key});
+
+  @override
+  State<_TestNavigationTabBar> createState() => _TestNavigationTabBarState();
+}
+
+class _TestNavigationTabBarState extends State<_TestNavigationTabBar>
+    with TickerProviderStateMixin {
+  late final TabController _tabBarController =
+      TabController(length: _testNames.length, vsync: this);
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO once backend: get list of tests that these projects have in common
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final List<ButtonSegment> buttonSegments = _buildButtonSegmentList();
+    final List<Tab> testTabList = _buildTestTabList();
+
+    return Container(
+      height: 58,
+      width: double.maxFinite,
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Colors.blue[900]!,
+            Colors.blueAccent,
+          ],
+        ),
+      ),
+      child: TabBar(
+        controller: _tabBarController,
+        isScrollable: true,
+        tabAlignment: TabAlignment.start,
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.white60,
+        dividerColor: Colors.transparent,
+        labelStyle: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+        labelPadding: EdgeInsets.symmetric(horizontal: 0),
+        padding: EdgeInsets.symmetric(horizontal: 4),
+        indicator: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+        ),
+        tabs: testTabList,
+      ),
+    );
+  }
+
+  List<Tab> _buildTestTabList() {
+    List<Tab> result = [];
+
+    for (var value in _testNames) {
+      result.add(
+        Tab(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 80),
+            child: Text(
+              value,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return result;
+  }
+
+  @override
+  void dispose() {
+    _tabBarController.dispose();
+    super.dispose();
   }
 }
