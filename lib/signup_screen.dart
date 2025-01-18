677e29ff68939758a91d73e423533d48a8fa9b40
Ryan Giunta <RyanJGiunta@gmail.com>
2025-01-17 19:47:54 -0500

    Add backend functionality to sign up screen

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:p2bp_2025spring_mobile/widgets.dart';
 import 'theme.dart';
 import 'login_screen.dart';
 import 'home_screen.dart';
@@ -90,155 +93,7 @@ class SignUpScreen extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 20),
-            // Full Name Input
-            const TextField(
-              decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(left: 10, right: 30),
-                  child: ImageIcon(
-                    AssetImage('assets/User_box.png'),
-                    color: Colors.white,
-                  ),
-                ),
-                labelText: 'Full Name',
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                filled: false,
-              ),
-            ),
-            const SizedBox(height: 10),
-            // Email Address Input
-            const TextField(
-              decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(left: 10, right: 30),
-                  child: Opacity(
-                    opacity: 0.75,
-                    child: ImageIcon(
-                      AssetImage('assets/mail_icon.png'),
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                labelText: 'Email Address',
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                filled: false,
-              ),
-            ),
-            const SizedBox(height: 10),
-            // Password Input
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                    left: 10,
-                    right: 30,
-                  ),
-                  child: ImageIcon(
-                    AssetImage('assets/Unlock.png'),
-                    color: Colors.white,
-                  ),
-                ),
-                labelText: 'Password',
-                labelStyle: const TextStyle(color: Colors.white),
-                focusedBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                border: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                filled: false,
-                suffixIcon: IconButton(
-                  icon: const Icon(
-                    Icons.visibility,
-                    color: Colors.grey,
-                  ),
-                  onPressed: () {
-                    // Toggle password visibility
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            // Confirm Password Input
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(
-                    left: 10,
-                    right: 30,
-                  ),
-                  child: ImageIcon(
-                    AssetImage('assets/Lock.png'),
-                    color: Colors.white,
-                  ),
-                ),
-                labelText: 'Confirm Password',
-                labelStyle: const TextStyle(color: Colors.white),
-                focusedBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                border: const UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                filled: false,
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.visibility, color: Colors.grey),
-                  onPressed: () {
-                    // Toggle password visibility
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            // Sign Up Button
-            ElevatedButton(
-              onPressed: () {
-                // Handle sign up logic
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const HomeScreen(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFFFCC00),
-                padding: EdgeInsets.symmetric(vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              child: const Text(
-                'Sign Up',
-                style: TextStyle(fontSize: 18, color: Colors.black),
-              ),
-            ),
+            SignUpForm(),
             const SizedBox(height: 20),
             // Already have an account redirect
             Center(
@@ -280,6 +135,265 @@ class SignUpScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _fullNameController = TextEditingController(),
+      _emailController = TextEditingController(),
+      _passwordController = TextEditingController(),
+      _confirmPasswordController = TextEditingController();
+  bool _obscureText = true;
+  bool _obscureConfirmText = true;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Validation feedback variables
+  bool _hasUpperCase = false;
+  bool _hasLowerCase = false;
+  bool _hasDigits = false;
+  bool _hasSpecialCharacter = false;
+  bool _isLengthValid = false;
+  bool _isTypingPassword = false;
+
+  @override
+  void dispose() {
+    _fullNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  // Register user with Firebase
+  Future<void> _registerUser() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        // Create user with email and password
+        UserCredential userCredential =
+            await _auth.createUserWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+
+        // Update the user's displayName with the full name entered during registration
+        await userCredential.user
+            ?.updateProfile(displayName: _fullNameController.text.trim());
+
+        // Add user data to Firestore
+        await _firestore.collection('users').doc(userCredential.user?.uid).set({
+          'fullName': _fullNameController.text.trim(),
+          'email': _emailController.text.trim(),
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+
+        // Send email verification
+        if (userCredential.user != null) {
+          await userCredential.user!.sendEmailVerification();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Registration successful! A verification email has been sent to ${_emailController.text.trim()}. Please verify your email before logging in.',
+              ),
+            ),
+          );
+        }
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('User Registered Successfully!')),
+        );
+
+        Navigator.pop(context);
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to register: $e')),
+        );
+      }
+    }
+  }
+
+  void _checkPasswordConditions(String value) {
+    setState(() {
+      _isTypingPassword = true;
+      _hasUpperCase = value.contains(RegExp(r'[A-Z]'));
+      _hasLowerCase = value.contains(RegExp(r'[a-z]'));
+      _hasDigits = value.contains(RegExp(r'[0-9]'));
+      _hasSpecialCharacter = value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+      _isLengthValid = value.length >= 12;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // Full Name Input
+          TextFormField(
+            controller: _fullNameController,
+            decoration: InputDecoration(
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(left: 10, right: 30),
+                child: ImageIcon(
+                  AssetImage('assets/User_box.png'),
+                  color: Colors.white,
+                ),
+              ),
+              labelText: 'Full Name',
+              labelStyle: TextStyle(color: Colors.white),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              filled: false,
+            ),
+          ),
+          const SizedBox(height: 10),
+          // Email Address Input
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(left: 10, right: 30),
+                child: Opacity(
+                  opacity: 0.75,
+                  child: ImageIcon(
+                    AssetImage('assets/mail_icon.png'),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              labelText: 'Email Address',
+              labelStyle: TextStyle(color: Colors.white),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              filled: false,
+            ),
+          ),
+          const SizedBox(height: 10),
+          // Password Input
+          PasswordTextFormField(
+            controller: _passwordController,
+            obscureText: _obscureText,
+            onChanged: _checkPasswordConditions,
+            decoration: InputDecoration(
+              prefixIcon: const Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  right: 30,
+                ),
+                child: ImageIcon(
+                  AssetImage('assets/Unlock.png'),
+                  color: Colors.white,
+                ),
+              ),
+              labelText: 'Password',
+              labelStyle: const TextStyle(color: Colors.white),
+              focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              border: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              filled: false,
+              suffixIcon: IconButton(
+                icon: const Icon(
+                  Icons.visibility,
+                  color: Colors.grey,
+                ),
+                onPressed: () {
+                  // Toggle password visibility
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          // Confirm Password Input
+          PasswordTextFormField(
+            controller: _confirmPasswordController,
+            obscureText: _obscureConfirmText,
+            decoration: InputDecoration(
+              prefixIcon: const Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                  right: 30,
+                ),
+                child: ImageIcon(
+                  AssetImage('assets/Lock.png'),
+                  color: Colors.white,
+                ),
+              ),
+              labelText: 'Confirm Password',
+              labelStyle: const TextStyle(color: Colors.white),
+              focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              border: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              filled: false,
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.visibility, color: Colors.grey),
+                onPressed: () {
+                  // Toggle password visibility
+                  setState(() {
+                    _obscureConfirmText = !_obscureConfirmText;
+                  });
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          // Sign Up Button
+          ElevatedButton(
+            onPressed: _registerUser,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFFFFCC00),
+              padding: EdgeInsets.symmetric(vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            child: const Text(
+              'Sign Up',
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+          ),
+        ],
       ),
     );
   }
