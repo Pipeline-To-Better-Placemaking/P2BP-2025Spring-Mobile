81195d176a1bfe025f312a7906ae91de2a7154e2
michaelsoffline <cuberomichael13@gmail.com>
2025-01-26 01:43:32 -0500

    Add updates to ios specific settings so that the project is compatible with iOS emulator and TestFlight. Commented out reference to firebase file that does not yet exist in current build.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'create_project_details.dart';
@@ -25,8 +25,8 @@ void main() async {
   try {
     // Ensure Firebase is initialized correctly
     await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+        // options: DefaultFirebaseOptions.currentPlatform,
+        );
     // Set Firebase persistence to SESSION to log out on tab/browser close
     await FirebaseAuth.instance.setPersistence(Persistence.SESSION);
   } catch (e) {
